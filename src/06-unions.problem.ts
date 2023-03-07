5b068d1c942e6e850ec18b073b8099007313cd76
Andrew Larder <andrew.c.larder@protonmail.com>
2023-03-07 23:14:42 +0800

    finished exercises 1-10

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -2,13 +2,7 @@ interface User {
   id: number;
   firstName: string;
   lastName: string;
-  /**
-   * How do we ensure that role is only one of:
-   * - 'admin'
-   * - 'user'
-   * - 'super-admin'
-   */
-  role: string;
+  role: 'admin' | 'user' |'super-admin'
 }
 
 export const defaultUser: User = {
--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -17,8 +17,14 @@ interface Post {
  * How do we ensure that makeUser ALWAYS
  * returns a user?
  */
-const makeUser = () => {
-  return {};
+const makeUser = (): User => {
+  return {
+    id: 1,
+    firstName: 'Andrew',
+    lastName: 'Larder',
+    role: 'admin',
+    posts: [{id: 1, title: 'test'}]
+  };
 };
 
 it("Should return a valid user", () => {
